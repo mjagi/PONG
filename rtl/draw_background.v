cecc382abb6fa5ce9124aa9b17f4b2f8cac95ebc
BartoszBialkowski <bartoszbialkowski19@gmail.com>
2022-07-08 16:28:57 +0200

    background recolorisation

--- rtl/draw_background.v
+++ rtl/draw_background.v
@@ -39,53 +39,18 @@ module draw_background (
     if (vblnk_in || hblnk_in) rgb_nxt <= 12'h0_0_0; 
     else
     begin
-      // Active display, top edge, make a yellow line.
-      if (vcount_in == 0) rgb_nxt <= 12'hf_f_0;
-      // Active display, bottom edge, make a red line.
-      else if (vcount_in == 599) rgb_nxt <= 12'hf_0_0;
+      // Active display, top edge, make a white line.
+      if (vcount_in == 0) rgb_nxt <= 12'hf_f_f;
+      // Active display, bottom edge, make a white line.
+      else if (vcount_in == 599) rgb_nxt <= 12'hf_f_f;
       // Active display, left edge, make a green line.
       else if (hcount_in == 0) rgb_nxt <= 12'h0_f_0;
-      // Active display, right edge, make a blue line.
-      else if (hcount_in == 799) rgb_nxt <= 12'h0_0_f;
+      // Active display, right edge, make a red line.
+      else if (hcount_in == 799) rgb_nxt <= 12'hf_0_0;
       // Active display, interior, fill with gray.
-      
-       //szkielet prosty liczby
-           else if (hcount_in >= 50 && hcount_in < 52 && vcount_in >= 50 && vcount_in < 151) rgb_nxt <= 12'hf_f_f;
-           else if (hcount_in >= 70 && hcount_in < 72 && ((vcount_in >= 65 && vcount_in < 86) || (vcount_in >= 115 && vcount_in < 136))) rgb_nxt <= 12'hf_f_f;
-           else if (hcount_in >= 50 && hcount_in < 61 && ((vcount_in >= 50 && vcount_in < 52) || (vcount_in >= 149 && vcount_in < 151)|| (vcount_in >= 97 && vcount_in < 99))) rgb_nxt <= 12'hf_f_f;
-           //górny brzuch
-           else if (hcount_in >= 61 && hcount_in < 63 && ((vcount_in >= 52 && vcount_in < 55)|| (vcount_in >= 99 && vcount_in < 102))) rgb_nxt <= 12'hf_f_f;
-           else if (hcount_in >= 63 && hcount_in < 65 && ((vcount_in >= 55 && vcount_in < 58)|| (vcount_in >= 102 && vcount_in < 105))) rgb_nxt <= 12'hf_f_f;
-           else if (hcount_in >= 65 && hcount_in < 67 && ((vcount_in >= 58 && vcount_in < 61)|| (vcount_in >= 105 && vcount_in < 108))) rgb_nxt <= 12'hf_f_f;
-           else if (hcount_in >= 67 && hcount_in < 69 && ((vcount_in >= 61 && vcount_in < 64)|| (vcount_in >= 108 && vcount_in < 111))) rgb_nxt <= 12'hf_f_f;
-           else if (hcount_in >= 69 && hcount_in < 71 && ((vcount_in >= 64 && vcount_in < 65)|| (vcount_in >= 111 && vcount_in < 115))) rgb_nxt <= 12'hf_f_f;
-           //dolny brzuch
-           else if (hcount_in >= 69 && hcount_in < 71 && ((vcount_in >= 86 && vcount_in < 89)|| (vcount_in >= 136 && vcount_in < 139))) rgb_nxt <= 12'hf_f_f;
-           else if (hcount_in >= 67 && hcount_in < 69 && ((vcount_in >= 89 && vcount_in < 92)|| (vcount_in >= 139 && vcount_in < 142))) rgb_nxt <= 12'hf_f_f;
-           else if (hcount_in >= 65 && hcount_in < 67 && ((vcount_in >= 92 && vcount_in < 95)|| (vcount_in >= 142 && vcount_in < 145))) rgb_nxt <= 12'hf_f_f;
-           else if (hcount_in >= 63 && hcount_in < 65 && ((vcount_in >= 95 && vcount_in < 98)|| (vcount_in >= 145 && vcount_in < 148))) rgb_nxt <= 12'hf_f_f;
-           else if (hcount_in >= 61 && hcount_in < 63 && ((vcount_in >= 98 && vcount_in < 101)|| (vcount_in >= 148 && vcount_in < 151))) rgb_nxt <= 12'hf_f_f;
-           
-           //dróga liczba
-           //szkielet prosty liczby
-           else if (hcount_in >= 80 && hcount_in < 82 && vcount_in >= 50 && vcount_in < 151) rgb_nxt <= 12'hf_f_f;
-           else if (hcount_in >= 100 && hcount_in < 102 && ((vcount_in >= 65 && vcount_in < 86) || (vcount_in >= 115 && vcount_in < 136))) rgb_nxt <= 12'hf_f_f;
-           else if (hcount_in >= 80 && hcount_in < 91 && ((vcount_in >= 50 && vcount_in < 52) || (vcount_in >= 149 && vcount_in < 151)|| (vcount_in >= 97 && vcount_in < 99))) rgb_nxt <= 12'hf_f_f;
-           //górny brzuch
-           else if (hcount_in >= 91 && hcount_in < 93 && ((vcount_in >= 52 && vcount_in < 55)|| (vcount_in >= 99 && vcount_in < 102))) rgb_nxt <= 12'hf_f_f;
-           else if (hcount_in >= 93 && hcount_in < 95 && ((vcount_in >= 55 && vcount_in < 58)|| (vcount_in >= 102 && vcount_in < 105))) rgb_nxt <= 12'hf_f_f;
-           else if (hcount_in >= 95 && hcount_in < 97 && ((vcount_in >= 58 && vcount_in < 61)|| (vcount_in >= 105 && vcount_in < 108))) rgb_nxt <= 12'hf_f_f;
-           else if (hcount_in >= 97 && hcount_in < 99 && ((vcount_in >= 61 && vcount_in < 64)|| (vcount_in >= 108 && vcount_in < 111))) rgb_nxt <= 12'hf_f_f;
-           else if (hcount_in >= 99 && hcount_in < 101 && ((vcount_in >= 64 && vcount_in < 65)|| (vcount_in >= 111 && vcount_in < 115))) rgb_nxt <= 12'hf_f_f;
-           //dolny brzuch
-           else if (hcount_in >= 99 && hcount_in < 101 && ((vcount_in >= 86 && vcount_in < 89)|| (vcount_in >= 136 && vcount_in < 139))) rgb_nxt <= 12'hf_f_f;
-           else if (hcount_in >= 97 && hcount_in < 99 && ((vcount_in >= 89 && vcount_in < 92)|| (vcount_in >= 139 && vcount_in < 142))) rgb_nxt <= 12'hf_f_f;
-           else if (hcount_in >= 95 && hcount_in < 97 && ((vcount_in >= 92 && vcount_in < 95)|| (vcount_in >= 142 && vcount_in < 145))) rgb_nxt <= 12'hf_f_f;
-           else if (hcount_in >= 93 && hcount_in < 95 && ((vcount_in >= 95 && vcount_in < 98)|| (vcount_in >= 145 && vcount_in < 148))) rgb_nxt <= 12'hf_f_f;
-           else if (hcount_in >= 91 && hcount_in < 93 && ((vcount_in >= 98 && vcount_in < 101)|| (vcount_in >= 148 && vcount_in < 151))) rgb_nxt <= 12'hf_f_f;
 
       // You will replace this with your own test.
-      else rgb_nxt <= 12'h8_8_8;    
+      else rgb_nxt <= 12'h0_0_0;    
     end
   end
   
