4dd25b8951142b1a48fda89bf552fe3a72b16e90
BartoszBialkowski <bartoszbialkowski19@gmail.com>
2022-07-08 16:42:21 +0200

    background2

--- rtl/draw_background.v
+++ rtl/draw_background.v
@@ -47,9 +47,7 @@ module draw_background (
       else if (hcount_in == 0) rgb_nxt <= 12'h0_f_0;
       // Active display, right edge, make a red line.
       else if (hcount_in == 799) rgb_nxt <= 12'hf_0_0;
-      // Active display, interior, fill with gray.
-
-      // You will replace this with your own test.
+      // Active display, interior, fill with black.
       else rgb_nxt <= 12'h0_0_0;    
     end
   end
